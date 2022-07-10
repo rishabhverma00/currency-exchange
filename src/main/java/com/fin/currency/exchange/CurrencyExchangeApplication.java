1ac03d46e3763bb1e2f6585ccaa2318dd9a2e6d1
rishabhverma0 <rishabh.verma@priceline.com>
2022-07-10 11:59:42 +0530

    basic redis commit

--- src/main/java/com/fin/currency/exchange/CurrencyExchangeApplication.java
+++ src/main/java/com/fin/currency/exchange/CurrencyExchangeApplication.java
@@ -3,13 +3,27 @@ package com.fin.currency.exchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 public class CurrencyExchangeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CurrencyExchangeApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CurrencyExchangeApplication.class, args);
+  }
 
+  @Bean
+  public JedisConnectionFactory jedisConnectionFactory() {
+    return new JedisConnectionFactory();
+  }
+
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate() {
+    RedisTemplate<String, Object> template = new RedisTemplate<>();
+    template.setConnectionFactory(jedisConnectionFactory());
+    return template;
+  }
 }
